de4b607e82d831647a8def9e07687e7f1b778484
archana <archanaguthale103@gmail.com>
2022-06-15 15:36:58 +0530

    15-06-33 updated dyanamic sidebar(archana)

---
+++ selec_std.php
@@ -0,0 +1,19 @@
+<?php 
+include('connect.php');
+echo $course=$_POST['course'];
+$query1 = "SELECT * FROM newcourse where CourseName='$course'";
+                   $statement1 = $connect->prepare($query1);
+                   $statement1->execute();
+                  
+                   if($statement1->rowCount() > 0)
+                       {
+                           $result1 = $statement1->fetchAll();
+                      
+                            foreach($result1 as $row1)
+                        {
+                            echo '<option value="'.$row1['StudentNames'].'">'.$row1['StudentNames'].'</option>';
+                        }
+                    }else{
+                        echo '<option>no student</option>';
+                    }
+?>          
