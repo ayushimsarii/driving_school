75581cdcbc5cdca3ff1cda967d92099b9ef75455
Ayushi Bharti <ayushi260395@gmail.com>
2022-06-15 15:28:42 +0530

    changes on phase-view page and gardesheet

---
+++ insert_item.php
@@ -0,0 +1,26 @@
+<?php
+include('connect.php');
+if(isset($_POST["Insert_item"]))
+	{
+       if(empty($_POST["item"]))
+			{
+				$error = "<div class='alert alert-danger'>Item Name required..</div>";
+			}
+				else
+					{
+						$item = $_POST["item"];
+						foreach ($item as $key => $value) 
+						{
+							var_dump($value);						
+							$sql = "INSERT INTO itembank (item) VALUES ('".$value."')";
+
+							$statement = $connect->prepare($sql);
+
+							$statement->execute();
+
+					        }
+						$error ="<div class='alert alert-success'>Data inserted successfully..</div>";
+                        header("Location:gradesheet.php?error=".$error>"");
+					}
+			}
+?>
