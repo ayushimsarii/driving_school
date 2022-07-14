5c696d6fed5edb6860ade0e0ecced4b31b20897e
Ayushi Bharti <ayushi260395@gmail.com>
2022-07-14 11:57:55 +0530

    header

--- header.php
+++ header.php
@@ -50,11 +50,11 @@ $q1 = "SELECT * FROM homepage where user_id=$user_id";
     <meta name="viewport" 
           content="width=device-width, 
                    initial-scale=1" />
-  <!-- <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/bootstrap.css" rel="stylesheet">
   <script src="js/jquery.mim.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/> -->
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/> 
 </head>
 <!-- <style type="text/css">
   .navbar-dark .navbar-nav .nav-link 
