b74a4cdaae68fceadf4a482d9ae8f013f42c7e9a
Ayushi Bharti <ayushi260395@gmail.com>
2022-07-22 15:54:42 +0530

    footr

--- Grade Sheet/footer.php
+++ Grade Sheet/footer.php
@@ -1,97 +1,59 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link href="css/bootstrap.css" rel="stylesheet">
-  <script src="js/jquery.mim.js"></script>
-    <script src="js/popper.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-#foo
-{
-  margin-top:500px;
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   color: white;
+   text-align: center;
 }
+
 </style>
 </head>
 <body>
 
 
-
-
-<div class="container h-100" id="foo">
+<div class="footer bg-success">
   <!-- Footer -->
-<footer class="text-center text-lg-start bg-light text-muted">
+<footer class="text-center text-lg-start bg-success text-muted">
   <!-- Section: Social media -->
   <section
     class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
   >
     <!-- Left -->
     <div class="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
+      <h5 style="color:white;">Get connected with us on social networks:</h5>
     </div>
     <!-- Left -->
 
     <!-- Right -->
     <div>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-facebook-f"></i>
+        <i style="color:white;" class="fab fa-facebook-f"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-twitter"></i>
+        <i style="color:white;" class="fab fa-twitter"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-google"></i>
+        <i style="color:white;" class="fab fa-google"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
+        <i style="color:white;" class="fab fa-instagram"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-linkedin"></i>
+        <i style="color:white;" class="fab fa-linkedin"></i>
       </a>
       <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
+        <i style="color:white;" class="fab fa-github"></i>
       </a>
     </div>
     <!-- Right -->
   </section>
-  <!-- Section: Social media -->
-  <section>
-  <div class="container-fluid" style="border: 2px solid blue;">
-    <center>
-        <div class="row" style="border:3px solid black; width:100%;">
-              <div class="col-3" style="border:4px solid red;">
-                <h3>TOS</h3>
-                <span>Here you can use rows and columns to organize your footer content. Lorem ipsum
-                 dolor sit amet, consectetur adipisicing elit.</span>
-              </div>
-
-              <div class="col-3">
-                <h4>TOS</h4>
-                <span>Here you can use rows and columns to organize your footer content. Lorem ipsum
-                 dolor sit amet, consectetur adipisicing elit.</span>
-              </div>
-
-              <div class="col-3">
-                <h4>TOS</h4>
-                <span>Here you can use rows and columns to organize your footer content. Lorem ipsum
-                 dolor sit amet, consectetur adipisicing elit.</span>
-              </div>
-              <div class="col-3">
-                <h4>TOS</h4>
-                <span>Here you can use rows and columns to organize your footer content. Lorem ipsum
-                 dolor sit amet, consectetur adipisicing elit.</span>
-              </div>      
-        </div>
-  </center>
-</div>
-</section>
-
-  <!-- Section: Links  -->
-  
-  <!-- Copyright -->
-  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2021 Copyright:
-    <a class="text-reset fw-bold" href="https://Msarii.com/">Msarii.com</a>
-  </div>
+ 
   <!-- Copyright -->
 </footer>
 <!-- Footer -->
